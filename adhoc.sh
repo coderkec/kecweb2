dc3c9d039169e0a1822ac86e7a3153d2e6f05f2d
coderkec <noody123@naver.com>
2025-09-10 18:01:05 +0900

    Track only .yml files

--- adhoc.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-ansible all -m ansible.builtin.yum_repository \
--a 'file=MyBase name=MyBase description="base software" baseurl=http://192.168.10.10/pkg/BaseOS gpgcheck=false enabled=true'
-
-
-ansible all -m ansible.builtin.yum_repository \
--a 'file=MyAppStream name=MyAppStream description="Appstream software" baseurl=http://192.168.10.10/pkg/AppStream/ gpgcheck=no enabled=yes'
